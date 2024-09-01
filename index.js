48e70d052767a1e74c7dc32a6a6a0d8bd5c5204a
Tomonya <149913048+tomonya@users.noreply.github.com>
2024-09-01 12:35:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
++ var map = L.map('map').setView([36.575, 137.984], 6);
++ L.control.scale({ maxWidth: 150, position: 'bottomright', imperial: false }).addTo(map);
++ map.zoomControl.setPosition('topright');
++ 
++ $.getJSON("prefectures.geojson", function (data) {
++     L.geoJson(data, {}).addTo(map);
++ }); 
