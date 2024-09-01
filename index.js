bba5fbd9733c9c571e10e910b36e7de1e95e1582
Tomonya <149913048+tomonya@users.noreply.github.com>
2024-09-01 12:37:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-+ var map = L.map('map').setView([36.575, 137.984], 6);
-+ L.control.scale({ maxWidth: 150, position: 'bottomright', imperial: false }).addTo(map);
-+ map.zoomControl.setPosition('topright');
-+ 
-+ $.getJSON("prefectures.geojson", function (data) {
-+     L.geoJson(data, {}).addTo(map);
-+ }); 
+var map = L.map('map').setView([36.575, 137.984], 6);
+L.control.scale({ maxWidth: 150, position: 'bottomright', imperial: false }).addTo(map);
+map.zoomControl.setPosition('topright');
+
++ var PolygonLayer_Style_nerv = {
++     "color": "#ffffff",
++     "weight": 1.5,
++     "opacity": 1,
++     "fillColor": "#3a3a3a",
++     "fillOpacity": 1
++ }
+
+$.getJSON("prefectures.geojson", function (data) {
+    L.geoJson(data, {
++       style: PolygonLayer_Style_nerv
+    }).addTo(map);
+}); 
